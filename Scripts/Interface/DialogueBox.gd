7ec2b8b685b68da2d366c0f082ec20e985541b35
Antharuu <antho.eclips@gmail.com>
2020-11-17 15:18:31 +0100

    Optimisations des dialogues

--- Scripts/Interface/DialogueBox.gd
+++ Scripts/Interface/DialogueBox.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var dialog: Array = ["Hello world !", "J'adore les patates.", "Pas toi ?"]
+var dialog: PoolStringArray
 var dialog_index: int = 0
 var dialog_finished: bool = false
 
@@ -9,15 +9,26 @@ onready var dialog_arrow = $Arrow
 onready var dialog_tween = $Tween
 
 
-func _ready() -> void:
+func _ready():
+	Globals.DialogueBox = self
+
+
+# Debut d'un dialogue
+func start(new_dialog: PoolStringArray) -> void:
+	dialog_index = 0
+	dialog = new_dialog
 	Globals.is_dialogue = true
 	load_dialog()
+	visible = true
 
 
-func _process(_delta):
+func _process(_delta) -> void:
 	dialog_arrow.visible = dialog_finished
-	if Input.is_action_just_pressed("ui_accept"):
-		load_dialog()
+	if Input.is_action_just_pressed("ui_interact"):
+		if dialog_finished:
+			load_dialog()
+		else:
+			finish_dialog()
 
 
 # Charger le prochain dialogue
@@ -30,11 +41,19 @@ func load_dialog() -> void:
 			dialog_text, "percent_visible", 0, 1, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
 		)
 		dialog_tween.start()
-	else:
+	else:  # Si c'est le dernier dialogue
 		Globals.is_dialogue = false
-		queue_free()
+		visible = false
 	dialog_index += 1
 
 
+# Afficher l'integralite du dialogue en cours
+func finish_dialog() -> void:
+	dialog_tween.stop(dialog_text)
+	dialog_text.percent_visible = 1
+	dialog_finished = true
+
+
+# Lorsque le Tween est termine
 func on_tween_completed(_object, _key):
 	dialog_finished = true
